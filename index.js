5ade0fe1de1085ec2fde38f3432b15792deff02f
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-06-16 15:38:32 +0100

    refactor: provide a parameter to the control function to be able to pass another argument for moveDirection when making the game mobile-friendly later

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const startButton = document.getElementById('start')
 const scoreDisplay = document.getElementById('score-display')
 const gameField = document.getElementById("game-grid")
 const bestResDisplay = document.getElementById("best")
+const popup = document.getElementById("popup")
+const levelDisplay = document.getElementById("level-display")
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
@@ -13,64 +15,63 @@ let bestScore = 0
 let resultsArr = []
 let intervalTime = 1000
 let speed = 0.9
+let levelOne = true
+let level2 = false
+let levelThree = false
 let timerId = 0
-
+let storedResults
 
 
 
 function createGrid() {
-    //create 100 of these elements with a for loop
-    for (let i=0; i < width*width; i++) {
-     //create element
-    const square = document.createElement('div')
-    //add styling to the element
-    square.classList.add('square')
-    //put the element into our grid
-    grid.appendChild(square)
-    //push it into a new squares array    
-    squares.push(square)
-    }
     
+    for (let i=0; i < width*width; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        grid.appendChild(square)
+       squares.push(square)
+    }    
     squares[currentSnake[0]].classList.add("head-br")
     squares[currentSnake[0]].style.transform = "rotate(-90deg)"
- 
-}
+ }
 createGrid()
-
 
 currentSnake.forEach(index => {
     squares[index].classList.add('snake')
     squares[currentSnake[0]].classList.remove("snake")
 })
 
-let storedResults
 function startGame() {
+    
+    if(levelOne) {
+        speed = 0.9
+    }else if(level2){
+        speed = 0.8
+    }else{
+        speed = 0.7
+    }
+
    gameField.scrollIntoView()
    gameField.focus()
    gameField.style.opacity = "1"
    document.getElementById("popup").style.display = "none"
-    //remove the snake
+   
     currentSnake.forEach(index => squares[index].classList.remove('snake'))
-    //remove the apple
+
     squares[appleIndex].classList.remove("apple")
-     //remove the head
+
     squares[currentSnake[0]].classList.remove("head-br")
   
     clearInterval(timerId)
     currentSnake = [2,1,0]
     score = 0
-    //re add new score to browser
-    scoreDisplay.textContent = score
+    scoreDisplay.textContent = `${score}/14`
     direction = 1
     intervalTime = 1000
     generateApple()
-    //readd the class of snake to our new currentSnake
     currentSnake.forEach(index => squares[index].classList.add('snake'))
     squares[currentSnake[0]].classList.add("head-br")
     squares[currentSnake[0]].style.transform = "rotate(-90deg)"
-   
-
-   
     timerId = setInterval(move, intervalTime)
 }
 
@@ -78,10 +79,10 @@ function startGame() {
 function move() {
    gameField.focus({block:"center"})
     if (
-        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
-        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
-        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
-        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        (currentSnake[0] + width >= width*width && direction === width) || 
+        (currentSnake[0] % width === width-1 && direction === 1) || 
+        (currentSnake[0] % width === 0 && direction === -1) || 
+        (currentSnake[0] - width < 0 && direction === -width) ||
         squares[currentSnake[0] + direction].classList.contains('snake')
     ) {    
         gameField.style.opacity = "0.8"
@@ -93,102 +94,93 @@ function move() {
         bestResDisplay.innerText = bestScore
         return clearInterval(timerId)
     }
-    
 
-   
-    //remove last element from our currentSnake array
     const tail = currentSnake.pop()
-      
-    //remove styling from last element
     squares[tail].classList.remove('snake')
     squares[currentSnake[0]].classList.remove("head-br")
     
-    //add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction)
     squares[currentSnake[0]].classList.add("head-br")
     
-    //change the direction of the "head" when the snake is moving
-        if(direction === 1) {
-            squares[currentSnake[0]].style.transform = "rotate(-90deg)"
-        }else if(direction === -1) {
-            squares[currentSnake[0]].style.transform = "rotate(90deg)"
-        }else if(direction === -width) {
-            squares[currentSnake[0]].style.transform = "rotate(180deg)"
-        }
+    if(direction === 1) {
+        squares[currentSnake[0]].style.transform = "rotate(-90deg)"
+    }else if(direction === -1) {
+        squares[currentSnake[0]].style.transform = "rotate(90deg)"
+    }else if(direction === -width) {
+        squares[currentSnake[0]].style.transform = "rotate(180deg)"
+    }
 
-    //rotate only the head     
-        squares.map(square => {
-            if(!square.classList.contains("head-br")) 
-             square.style.transform = "rotate(0)"  
-        })
+     
+   squares.map(square => {
+       if(!square.classList.contains("head-br")) 
+        square.style.transform = "rotate(0)"  
+   })
 
-      
-   //add styling so we can see it
-    
-    //deal with snake head gets apple
+ 
     if (squares[currentSnake[0]].classList.contains('apple')) {
-        //remove the class of apple
+       
         squares[currentSnake[0]].classList.remove('apple')
-        //grow our snake by adding class of snake to it
         squares[tail].classList.add('snake')
-        //grow our snake array
         currentSnake.push(tail)
-        //generate new apple
         generateApple()
-        //add one to the score
         score++
-        //display our score
-        scoreDisplay.textContent = score
-        //speed up our snake
+        scoreDisplay.textContent = `${score}/14`
         clearInterval(timerId)
         intervalTime = intervalTime * speed
-         timerId = setInterval(move, intervalTime)
+        timerId = setInterval(move, intervalTime)
     }   
-    squares[currentSnake[0]].classList.add('snake')
-}
+    squares[currentSnake[0]].classList.add('snake') 
 
+}
 
 function generateApple() {
     do {
-        appleIndex = Math.floor(Math.random() * squares.length)
+    appleIndex = Math.floor(Math.random() * squares.length)
     } while (squares[appleIndex].classList.contains('snake'))
     squares[appleIndex].classList.add('apple')
 } 
 
-// 39 is right arrow
-// 38 is for the up arrow
-// 37 is for the left arrow
-// 40 is for the down arrow
-
-function control(e) {
-    if (e.keyCode === 39) {
-       direction = 1
-    } else if (e.keyCode === 38) {
-       direction = -width
-      
-    } else if (e.keyCode === 37) {
-       direction = -1
-    } else if (e.keyCode === 40) {
-       direction = +width
-    }
+function control(moveDirection) {
+    if (moveDirection === "ArrowRight") {
+        direction = 1;
+      }
+      if (moveDirection === "ArrowLeft") {
+        direction = -1;
+      }
+      if (moveDirection === "ArrowUp") {
+        direction = -width;
+      }
+      if (moveDirection === "ArrowDown") {
+        direction = width;
+      }
 }
-document.addEventListener('keyup', control)
-// startButton.addEventListener('click', startGame)
+
+function handleKeyMove(e) {
+    if (!["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"].includes(e.key))
+    return
+    control(e.key)
+}
+
+document.addEventListener('keyup', handleKeyMove)
 
 gameField.addEventListener("click", () => {
-
     startGame()
 })
 
-//storing best result
+function levelTwo() {
+    if(score === 1) {
+        levelOne = false
+        level2 = true
+        clearInterval(timerId)
+        popup.innerHTML = `<h2>Congratulations! Follow to the next level</h2>`
+        popup.style.display = "block"
+        setInterval(startGame,2000)
+    }
+}
 
 
-
-
-//store the best result in a local storage
 //create levels with a different starting speed
 //the last level will have the fastest speed but the walls will be permeable
 //depending on the best result, restart the game from the suitable level
 //change the design of "game-info section" name at the top and different features around
 //get rid of stat button (just notify the user to click on the field)
-
